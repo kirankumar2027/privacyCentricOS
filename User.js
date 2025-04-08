7450a0fdf7dc49b1eacd283b3647933a1840b8fe
kirankumar2027 <23je0795@iitism.ac.in>
2025-04-08 16:01:49 +0530

    Create User.js

---
+++ User.js
@@ -0,0 +1,10 @@
+// Privacy hardening settings
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("browser.send_pings", false);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("webgl.disabled", true);
+user_pref("media.peerconnection.enabled", false);
+user_pref("beacon.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
